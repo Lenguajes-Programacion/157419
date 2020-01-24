f3b9d1e7beca7f844b1b3ba16d25e9cbb3e35060
Ivan Francisco <60111398+TheGreatNavi@users.noreply.github.com>
2020-01-24 12:39:52 -0700

    Push
    Cambios

---
+++ clase/Practica_uno/Program.cs
@@ -0,0 +1,13 @@
+using System;
+namespace Practica_uno
+{
+    class Program
+    {
+        string nombre = "Frank";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo" + this.nombre);
+        }
+    }
+}
